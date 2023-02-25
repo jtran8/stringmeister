05fbe21acf1c86e95d292b727f1319a9ff95454a
jtran <jacky@tecium.com>
2023-02-25 02:24:20 -0500

    FIxed notfound loaded

--- src/lib/pages/home/components/NotFoundCard.tsx
+++ src/lib/pages/home/components/NotFoundCard.tsx
@@ -2,15 +2,19 @@ import { Card, CardBody, Image, Text } from "@chakra-ui/react";
 
 import sad from "../../../../assets/img/sad.webp";
 
-const NotFoundCard = () => {
+import type StringData from "./StringData";
+
+const NotFoundCard = ({ stringList }: StringData) => {
   return (
     <Card maxW="md">
-      <CardBody>
-        <Text fontSize="sm" mb="20px" textAlign="center">
-          String not found.
-        </Text>
-        <Image src={sad} borderRadius="lg" />
-      </CardBody>
+      {stringList && (
+        <CardBody>
+          <Text fontSize="sm" mb="20px" textAlign="center">
+            String not found.
+          </Text>
+          <Image src={sad} borderRadius="lg" />
+        </CardBody>
+      )}
     </Card>
   );
 };
